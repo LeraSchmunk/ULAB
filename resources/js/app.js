4fb1bd9251ad1453e82e6d2ca27039a0c0920d98
LeraSchmunk <vshmunk@mail.ru>
2023-11-06 19:17:34 +0300

    yandex metrika

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,14 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router';
+import VueYandexMetrika from 'vue3-yandex-metrika';
 
 
 createApp(App)
     .use(router)
+    .use(VueYandexMetrika, {
+        id: 94711045,
+        router: router,
+        env: process.env.NODE_ENV
+    })
     .mount("#app")
